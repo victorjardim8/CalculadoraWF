b260bd8ae153128df68be9e459749f61d48683d6
Victor Jardim <victor.jardim@gomara.tech>
2023-08-07 08:14:10 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite números a serem somados");
+
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"O resultado da soma é {Soma(numero1, numero2)}");
+
+
+            Console.WriteLine("Aperte Enter para finalizar o programa!");
+
+            Console.ReadLine();
+        }
+
+        public static int Soma(int numero1, int numero2)
+        {
+            return numero1 + numero2;
+        }
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(textBox1.Text);
+            int n2 = Convert.ToInt32(textBox2.Text);
+            int resultado = Soma(n1, n2);
+            MessageBox.Show($"O resultado da soma foi: {resultado}");
+        }
+
+        public static int Soma(int numero1, int numero2)
+        {
+            return numero1 + numero2;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
